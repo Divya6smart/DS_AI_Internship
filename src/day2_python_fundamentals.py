139c27cd40e902020789cb3a71d589123e0c34da
Divya <divyachoti415@gmail.comgit config --listgit config --global username Divyagit config --global user.email divyachoti415@gmail.com>
2026-02-03 15:26:08 +0530

    day2

---
+++ src/day2_python_fundamentals.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+name = input("Enter your name: ")
+age  = int(input("Enter your age: "))
+
+new_age = age + 4
+print(new_age)
+
+print("Hey", name, "you will be", new_age, "years old in 2030!")
+      
+bill = float(input("Enter total bill amount: "))
+people = int(input("Enter number of people: "))
+
+share = bill / people
+
+print("Total Bill:", bill, "Each person pays:", share)
+age
+
+item_name = "Laptop"
+quantity = 2
+price = 499.99
+in_stock = True
+
+print("Item:", item_name, "Qty:", quantity, "Price: ₹", price, "Available:", in_stock)
+
+total_cost = quantity * price
+print("Total Cost: ₹", total_cost)
