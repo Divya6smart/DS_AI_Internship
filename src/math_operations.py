305736a5ea342366a6258d8667201ae1825bc648
Divya <divyachoti415@gmail.comgit config --listgit config --global username Divyagit config --global user.email divyachoti415@gmail.com>
2026-02-06 15:20:32 +0530

    main

---
+++ src/math_operations.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  6 15:09:02 2026
+
+@author: divya
+"""
+
+def power(base, exp):
+    return base ** exp
+
+def average(numbers_list):
+    return sum(numbers_list) / len(numbers_list)
---
+++ src/untitled0.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  6 11:40:23 2026
+
+@author: divya
+"""
+
+def play_line():
+    print("welcome to internship")
+    
+play_line()  
+
+def classic_legend():
+    print("hello my name is Divya")
+    
+classic_legend()
+    
+
+def add(a,b):
+    return(a+b)
+    
+answer = add(20,10)    
+print(answer)
+
+
+
+
+    
+    
